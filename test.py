3ee2f8ebf53687fa63a2a97228ccba6b58910929
Nick Fox-Gieg <nick@fox-gieg.com>
2024-02-01 00:48:34 -0500

    testing

---
+++ test.py
@@ -0,0 +1,21 @@
+import trimesh
+import latk
+import neuralgas
+
+mesh = trimesh.load("test.ply")
+points = mesh.vertices
+
+# dataset, max_neurons=2000, max_iter=8, max_age=10, eb=0.1, en=0.006, alpha=0.5, beta=0.995, l=200
+gas = neuralgas.GrowingNeuralGas(points, max_neurons=10000, max_iter=100, max_age=10, eb=0.1, en=0.006, alpha=0.5, beta=0.999, l=200)
+gas.learn()
+
+la = latk.Latk(init=True)
+ls = latk.LatkStroke()
+
+for i in range(0, len(gas.gng.vs)):
+    point = gas.gng.vs[i]["weight"]
+    lp = latk.LatkPoint(co=(point[0], point[2], point[1]))
+    ls.points.append(lp)
+
+la.layers[0].frames[0].strokes.append(ls)
+la.write("test.latk")
