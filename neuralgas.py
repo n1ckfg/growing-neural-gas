56cbf0c87fd7b146b49aabcf4397bd7819fa792f
Nick Fox-Gieg <nick@fox-gieg.com>
2024-02-08 08:56:56 -0500

    Update neuralgas.py

--- neuralgas.py
+++ neuralgas.py
@@ -1,3 +1,23 @@
+# https://github.com/rendchevi/growing-neural-gas
+'''
+This is an implementation of Growing Neural Gas (GNG) algorithm, an unsupervised
+machine learning model based on Self-organizing Map (SOM) useful for learning
+topology of a data.
+
+This method commonly used for data clustering, but there has been some
+interesting research to use GNG for performing 3D reconstruction task from raw
+point cloud data such as acquired from a 3D scanner or LIDAR. You can learn
+more in here.
+https://ieeexplore.ieee.org/document/6889546
+
+The GNG module I wrote in this repo is part of my bachelor thesis which
+concerned on 3D reconstruction for 3D scanned medical data, you can check out
+some of my results below (3D full head reconstruction). I don't have any plan
+soon to improve or maintain this GNG module, feel free to tinker with the
+module, some major improvement is to integrate 3D reconstruction code within
+the module.
+'''
+
 from tqdm import tqdm
 from sys import stdout
 
