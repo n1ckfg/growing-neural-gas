7bdef9f36fec2a0ec393a7887e6131092351f412
Nick Fox-Gieg <nick@fox-gieg.com>
2024-02-08 13:52:56 -0500

    Update test005.py

--- test005.py
+++ test005.py
@@ -9,18 +9,26 @@ points = mesh.vertices
 gas = neuralgas.GrowingNeuralGas(points, max_neurons=100000, max_iter=100, max_age=10, eb=0.1, en=0.006, alpha=0.5, beta=0.995, l=20)
 gas.learn()
 
-la = latk.Latk(init=True)
+# get edges from indices
+edgeList = []
 
 for edge in gas.gng.es:
-    ls = latk.LatkStroke()
-    
     point1 = gas.gng.vs[edge.source]["weight"]
     point2 = gas.gng.vs[edge.target]["weight"]
-  
-    lp1 = latk.LatkPoint(co=(point1[0], point1[2], point1[1]))
-    lp2 = latk.LatkPoint(co=(point2[0], point2[2], point2[1]))
-    ls.points.append(lp1)
-    ls.points.append(lp2)
+    edgeList.append((point1, point2))
+
+# TODO merge edges that share points
+newEdgeList = edgeList
+
+# convert edge list to Latk
+la = latk.Latk(init=True)
+
+for edge in newEdgeList: 
+    ls = latk.LatkStroke()
+    
+    for point in edge:
+        lp = latk.LatkPoint(co=(point[0], point[2], point[1]))
+        ls.points.append(lp)
 
     la.layers[0].frames[0].strokes.append(ls)
 
